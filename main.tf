4d6b4c4ad88a5e327a46ec53b184890f7a89b447
Kento555 <shen_kento555@hotmail.com>
2025-04-02 12:28:06 +0800

    Fix: Terraform formatting & TFLint warnings

--- main.tf
+++ main.tf
@@ -1,23 +1,31 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket = "sctp-ce9-tfstate"
-    key    = "ws-s3-tf-ci.tfstate" #Change this
+    key    = "ws-s3-tf-ci.tfstate"
     region = "us-east-1"
   }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
 
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
   account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
 }
-
